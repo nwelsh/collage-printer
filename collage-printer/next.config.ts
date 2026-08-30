777198efb47ad29c603d3b480637a54911a16bbc
nwelsh <newelsh@wisc.edu>
2026-08-30 09:48:52 -0500

    deploy

--- collage-printer/next.config.ts
+++ collage-printer/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/collage-printer" : "",
+  assetPrefix: isProd ? "/collage-printer/" : "",
 };
 
 export default nextConfig;
